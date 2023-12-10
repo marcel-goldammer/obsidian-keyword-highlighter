2eb7cfff45958e8185f19065a90f53ca73b600fa
marcel-goldammer <marcel.goldammer@web.de>
2023-12-10 18:20:21 +0100

    rewrite replace function in reader mode to fix callout icon bug

--- src/reader-extension/reader-highlighter.ts
+++ src/reader-extension/reader-highlighter.ts
@@ -1,17 +1,56 @@
 import KeywordHighlighterPlugin from "main";
 import { MarkdownPostProcessor } from "obsidian";
-import { getCssClasses } from "src/shared";
+import { KeywordStyle, getCssClasses } from "src/shared";
 
 export const readerHighlighter: MarkdownPostProcessor = (el: HTMLElement) => {
   KeywordHighlighterPlugin.settings.keywords
     .filter((keyword) => !!keyword.keyword)
-    .forEach((keyword) => {
-      const regex = new RegExp(`(${keyword.keyword}:)`, "gi");
-      el.innerHTML = el.innerHTML.replace(
-        regex,
-        `<span class="${getCssClasses(keyword)}" style="--kh-c: ${
-          keyword.color
-        }; --kh-bgc: ${keyword.backgroundColor}">$1</span>`
-      );
-    });
+    .forEach((keyword) => replaceWithHighlight(el, keyword));
 };
+
+function replaceWithHighlight(node: Node, keyword: KeywordStyle) {
+  if (
+    // skip highlighting nodes
+    node.nodeType === Node.ELEMENT_NODE &&
+    (<Element>node).classList.contains("kh-highlighted")
+  ) {
+    return;
+  } else if (node.nodeType === Node.TEXT_NODE && node.nodeValue) {
+    const searchText = `${keyword.keyword}:`;
+    const index = node.nodeValue.indexOf(searchText);
+    if (index > -1) {
+      // parent cannot be null
+      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+      const parent = node.parentNode!;
+      const beforeText = node.nodeValue.substring(0, index);
+      const afterText = node.nodeValue.substring(index + searchText.length);
+      const highlight = getHighlightNode(parent, searchText, keyword);
+      // insert order: <beforeText> <highlight> <afterText>
+      parent.insertBefore(document.createTextNode(beforeText), node);
+      parent.insertBefore(highlight, node);
+      node.nodeValue = afterText;
+      // we have to call the function again for all sibling nodes
+      // to find all keyword occurances
+      parent.childNodes.forEach((child) =>
+        replaceWithHighlight(child, keyword)
+      );
+    }
+
+    return;
+  }
+  // call recursively
+  node.childNodes.forEach((child) => replaceWithHighlight(child, keyword));
+}
+
+function getHighlightNode(
+  parent: Node,
+  searchText: string,
+  keyword: KeywordStyle
+): Node {
+  const highlight = parent.createSpan();
+  highlight.classList.add(...getCssClasses(keyword).split(" "));
+  highlight.style.setProperty("--kh-c", keyword.color);
+  highlight.style.setProperty("--kh-bgc", keyword.backgroundColor);
+  highlight.setText(searchText);
+  return highlight;
+}
