61f1d711efedf0ca83bdc08d85a3895e505703c6
Marcel Goldammer <marcel.goldammer@web.de>
2026-02-07 18:37:01 +0100

    chore(01-02): install and configure git hooks
    - Install husky, lint-staged, @commitlint/cli, @commitlint/config-conventional
    - Initialize Husky with prepare script
    - Create pre-commit hook running lint-staged
    - Create commit-msg hook running commitlint
    - Configure commitlint with extended type set and optional scope
    - Configure lint-staged to run Prettier then ESLint sequentially

---
+++ commitlint.config.js
@@ -0,0 +1,10 @@
+export default {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [2, 'always', ['build', 'chore', 'ci', 'docs', 'feat', 'fix', 'perf', 'refactor', 'revert', 'style', 'test']],
+    'scope-empty': [0],
+    'subject-case': [2, 'always', 'lower-case'],
+    'header-max-length': [2, 'always', 100],
+    'body-max-line-length': [2, 'always', 100],
+  },
+};
