fe9e5993e48461c003bfc95ee4610cefdb7254c6
Marcel Goldammer <marcel.goldammer@web.de>
2026-02-08 08:06:03 +0100

    feat(02-02): restructure setting tab with collapsible sections
    - add CollapsibleSection import and usage for two-section layout
    - global settings section collapsed by default with placeholder description
    - keywords section expanded by default with reactive keyword count badge
    - preserve all existing keyword rendering and add/remove functionality

--- src/settings/SettingTab.svelte
+++ src/settings/SettingTab.svelte
@@ -1,12 +1,14 @@
 <script lang="ts">
   import type { KeywordStyle } from 'src/shared';
   import KeywordSetting from './KeywordSetting.svelte';
+  import CollapsibleSection from './CollapsibleSection.svelte';
   import type { Writable } from 'svelte/store';
   import { addKeyword, removeKeyword, type PluginSettings } from 'src/stores/settings-store';
 
   export let settingsStore: Writable<PluginSettings>;
 
   $: keywords = $settingsStore.keywords;
+  $: keywordCount = keywords.length;
 
   // eslint-disable-next-line no-undef
   let ref: HTMLElement;
@@ -21,13 +23,21 @@
 </script>
 
 <div bind:this={ref}>
-  {#each keywords as keyword, index}
-    <KeywordSetting {index} {keyword} on:remove={() => handleRemoveKeyword(keyword)} />
-  {/each}
+  <CollapsibleSection
+    title="Global Settings"
+    description="Global settings will be added in future updates. This section will include options like case sensitivity and pattern matching that apply to all keywords."
+    expanded={false}
+  />
 
-  <div class="setting-item">
-    <button on:click={handleAddKeyword}>Add new keyword</button>
-  </div>
+  <CollapsibleSection title="Keywords" badge="({keywordCount})" expanded={true}>
+    {#each keywords as keyword, index}
+      <KeywordSetting {index} {keyword} on:remove={() => handleRemoveKeyword(keyword)} />
+    {/each}
+
+    <div class="setting-item">
+      <button on:click={handleAddKeyword}>Add new keyword</button>
+    </div>
+  </CollapsibleSection>
 </div>
 
 <style></style>
